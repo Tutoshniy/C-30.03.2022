fd5a0a4a4d61a7ea3169bb6b2e626e035ab97abb
Gleb <gleb.gadbrat@mail.ru>
2022-04-01 16:01:39 +0300

    Задание 3 - Написать программу масштабирования фигуры

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество точек у фигуры");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите коэффициент масштабирования");
+int k = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i<=n; i++)
+{
+    Console.WriteLine("Введите координаты точки");
+    int x = Convert.ToInt32(Console.ReadLine());
+    int y = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Получившиеся координаты:" + x*k + ", " + y*k);
+}
