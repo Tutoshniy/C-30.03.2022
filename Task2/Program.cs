721f9a500540892e44ba83035088084c34b6b978
Gleb <gleb.gadbrat@mail.ru>
2022-04-01 15:42:34 +0300

    Задание 2 - Показать числа Фибоначчи

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите конечное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i1 = 0;
+int i2 = 1;
+Console.Write(i1 + " " + i2 + " ");
+int i = 0;
+while (i1 + i2 < n)
+{
+    i = i1 + i2;
+    i1 = i2;
+    i2 = i;
+    Console.Write(i + " ");
+}
